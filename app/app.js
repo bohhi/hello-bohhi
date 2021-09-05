0064c9af62ccc9312396cdb34f79674da5f3dc97
Diana Kulyk <bohhi@hotmail.com>
2021-09-05 11:13:26 +0200

    update

--- app/app.js
+++ app/app.js
@@ -1,13 +1,6 @@
-var progress = document.querySelector('.progress')
-var count = 4;
-var loading = setInterval(animate, 50)
-function animate(){
-    if(count == 100 && per == 400){
-        clearInterval(loading);
-    }else{
-        per = per + 4;
-        count = count + 1;
-        progress.getElementsByClassName.width = per + 'px';
-        
+document.onreadystatechange = function () {
+    if (document.readyState === "complete") {
+        console.log(document.readyState);
+        document.getElementById("PreLoaderBar").style.display = "none";
     }
 }
